3a1847d934ad6cb0824a49d7ccd3dedb823c06ee
Oliver Lie <oliverlie@krystal3d.com>
2026-08-20 19:34:38 -0700

    Finished "Clause VSIDS"

---
+++ 2_CDCL/9_Restarts/src/SAT.hpp
@@ -0,0 +1,119 @@
+#pragma once
+
+#ifndef SAT_H
+#define SAT_H
+
+#include <algorithm>
+#include <bitset>
+#include <optional>
+#include <queue>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
+
+namespace saturn {
+    class satsolver {
+        public:
+            satsolver(std::string_view input);
+            bool solveCNF();
+
+        private:
+            //we need 2 bits to represent a variable's assignment, 
+            // and I'll be damned if I use 8 bits instead of 2.
+            typedef std::bitset<2> var;
+
+            //The three states a variable can have
+            static constexpr var FALSE{0b00};
+            static constexpr var TRUE{0b01};
+            static constexpr var UNASSIGNED{0b10};
+
+            /// @brief Evaluates whether or not a variable evaluates to False
+            /// @param var a variable
+            /// @return True iff the passed in variable would evaluate to False
+            ///             False otherwise
+            constexpr bool evals_false(int var) {
+                int idx = std::abs(var) - 1;
+                return var > 0 && vars[idx] == FALSE || var < 0 && vars[idx] == TRUE;
+            };
+
+            /// @brief Maps a variable to an index in var_to_clause
+            /// @param var a variable
+            /// @return an index in var_to_clause 
+            constexpr size_t var_to_widx(int lit) {
+                size_t var = std::abs(lit) - 1;
+                return 2 * var + (lit < 0);
+            };
+
+            struct TrailEntry {
+                //the literal being assigned (eg, 5 or -5)
+                int lit;
+
+                //In order to implement backjumping, we need to formalize decision levels now
+                size_t decisionLevel;
+
+                //the index of the clause for why it was assigned that way
+                //reasonIdx.hasValue() == False => decision, else => propagation
+                std::optional<size_t> reasonIdx;
+            };
+
+            void parse(std::string_view equation);
+
+            std::optional<std::vector<int>> propagate(size_t decisionLevel);
+
+            bool createWatched();
+
+            bool createWatched(size_t index, size_t decisionLevel);
+
+            //helpers:
+            void removeFromWatchList(std::vector<size_t>& watchList, size_t clauseIdx);
+            void removeClauseFromWatchLists(size_t clauseIdx);
+            void replaceWatchIndex(size_t oldIdx, size_t newIdx);
+
+            size_t numVars{};
+            size_t numClauses{};
+            size_t maxSize{};
+            size_t qHead{};
+
+            std::vector<std::vector<int>> clauses;
+            std::vector<var> vars;
+
+            //index i is whether or not we've seen variable i + 1 in clause resolution
+            std::vector<bool> seen;
+            std::vector<bool> delta;
+            std::vector<double> activity;
+            double lbdema{};
+
+            //data structures for "clause VSIDS"
+            double activeInc = 1.0;
+            double activeGrow = 1.05;
+            std::vector<double> cActivity;
+            std::vector<size_t> creationConflict;
+            std::vector<size_t> lbds;
+
+            //index i is an index into seen
+            std::vector<size_t> seenIdx;
+
+            //index i is the position of an index in seenIdx
+            std::vector<size_t> seenPos;
+
+            //holds information about our decision trail
+            std::vector<TrailEntry> trail;
+
+            //each element is an index into trail where a new decision level starts
+            std::vector<size_t> trail_starts;
+
+            //This is a map where var_data[i] = the index in the trail that variable i 
+            //  appears in
+            std::vector<std::optional<size_t>> var_to_trail;
+
+            //map from variable to clause indices where 
+            //  var_to_clause[var_to_widx(var)] = the vector of indices that var is watching
+            std::vector<std::vector<size_t>> var_to_clause;
+
+            //map from clause index to variable indices where
+            //  clause_to_var[i] = a pair of indices to literals that are watching clause i
+            std::vector<std::pair<size_t, size_t>> clause_to_var;
+    };
+}
+
+#endif
---
+++ 2_CDCL/9_Restarts/tests/Satisfiable.cpp
@@ -0,0 +1,59 @@
+#include <testpp/testpp.hpp>
+#include <filesystem>
+
+#include "../src/SAT.hpp"
+
+TEST(satisfiable, uf20_91) {
+    int idx = 0;
+    int max = 1;
+    
+    for (const auto& path : std::filesystem::directory_iterator("test_sets/uf20-91")) {
+        // if (idx == max) {
+        //     break;
+        // }
+        // idx++;
+        std::cout << path.path() << '\n';
+        saturn::satsolver solver(path.path().c_str());
+        bool solved = solver.solveCNF();
+        ASSERT_TRUE(solved);
+    }
+
+    // saturn::satsolver solver("test_sets/uf20-91/uf20-0778.cnf");
+    // bool solved = solver.solveCNF();
+    // ASSERT_TRUE(solved);
+}
+
+TEST(satisfiable, Flat200_470) {
+    int idx = 0;
+    int max = 1;
+    
+    for (const auto& path : std::filesystem::directory_iterator("test_sets/Flat200-479")) {
+        // if (idx == max) {
+        //     break;
+        // }
+        // idx++;
+        std::cout << path.path() << '\n';
+        saturn::satsolver solver(path.path().c_str());
+        bool solved = solver.solveCNF();
+        ASSERT_TRUE(solved);
+    }
+}
+
+TEST(unsatisfiable, UUF50_218_1000) {
+    int idx = 0;
+    int max = 1;
+    
+    for (const auto& path : std::filesystem::directory_iterator("test_sets/UUF50.218.1000")) {
+        // if (idx == max) {
+        //     break;
+        // }
+        // idx++;
+        std::cout << path.path() << '\n';
+        saturn::satsolver solver(path.path().c_str());
+        bool solved = solver.solveCNF();
+        ASSERT_FALSE(solved);
+    }
+    // saturn::satsolver solver("test_sets/UUF50.218.1000/uuf50-0410.cnf");
+    // bool solved = solver.solveCNF();
+    // ASSERT_FALSE(solved);
+}
